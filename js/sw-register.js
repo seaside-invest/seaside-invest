e303d26b8bc8cbcc1461656c58e92bf0f10194ca
seaside-invest <sondre@familien-andersen.com>
2025-08-06 13:56:48 +0200

    Delete js directory

--- js/sw-register.js
+++
@@ -1,6 +0,0 @@
-// Register Service Worker with version query to bust CDN caches
-(function() {
-  if (!('serviceWorker' in navigator)) return;
-  const v = '2025-08-05';
-  navigator.serviceWorker.register('/sw.js?v=' + v).catch(console.error);
-})();
